faf2a0766a2e478fb38ff77ab6ec734b60dd38b8
Mike Borozdin <nolongemails@gmail.com>
2016-10-11 18:38:24 -0700

    copy of the static config from Heroku sample

--- azureupload/settings.py
+++ azureupload/settings.py
@@ -118,9 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = '/staticfiles'
 
 AZURE_ACCOUNT_NAME = os.environ.get("AZURE_ACCOUNT_NAME")
 AZURE_ACCOUNT_KEY = os.environ.get("AZURE_ACCOUNT_KEY")
