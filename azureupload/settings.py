6ce9dcf634a0b21510a530d3637fbab80a7cbeb6
Mike Borozdin <nolongemails@gmail.com>
2016-10-11 18:32:55 -0700

    static fix for heroku deploy

--- azureupload/settings.py
+++ azureupload/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/staticfiles'
 
 AZURE_ACCOUNT_NAME = os.environ.get("AZURE_ACCOUNT_NAME")
 AZURE_ACCOUNT_KEY = os.environ.get("AZURE_ACCOUNT_KEY")
