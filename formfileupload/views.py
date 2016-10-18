a2d36f42e0fe51c3a64b9cdb1ddc6954ef2a2d96
Mike Borozdin <nolongemails@gmail.com>
2016-10-18 13:35:54 -0700

    working upload (with the help of Microsoft Support)

--- formfileupload/views.py
+++ formfileupload/views.py
@@ -6,17 +6,19 @@ from django.conf import settings
 import json
 import urlparse
 
+
 def index(request):
     template = loader.get_template('index.html')
     context = {}
 
     return HttpResponse(template.render(context, request))
 
+
 def signature(request, file_name):
     azure_utils = AzureUtils(settings.AZURE_ACCOUNT_NAME, settings.AZURE_ACCOUNT_KEY)
     signature = azure_utils.generate_access_signature(file_name)
     result = {
-        "bloburl": urlparse.urljoin("https://tempodevelop.blob.core.windows.net/tmp/", file_name),
+        "bloburl": urlparse.urljoin("https://tempodevelop.blob.core.windows.net/tmp/",file_name),
         "signature": signature,
-    }   
+    }
     return HttpResponse(json.dumps(result))
--- lib/storage/azure_utils.py
+++ lib/storage/azure_utils.py
@@ -1,13 +1,14 @@
-from azure.storage.blob import BlobService, BlobSharedAccessPermissions
-from azure.storage import AccessPolicy, SharedAccessPolicy
+from azure.storage.blob import BlockBlobService, ContainerPermissions
 from datetime import datetime, timedelta
+
 
 class AzureUtils:
 
     def __init__(self, account_name, account_key):
-        if account_name is None: raise ValueError("account_name should not be None")
-        if account_key is None: raise ValueError("account_key should not be None")
-        
+        if account_name is None:
+            raise ValueError("account_name should not be None")
+        if account_key is None:
+            raise ValueError("account_key should not be None")
         self.account_name = account_name
         self.account_key = account_key
 
@@ -15,18 +16,20 @@ class AzureUtils:
         """
         calls the Azure Web service to generate a temporary access signature.
         """
-        blob_service = BlobService(
-            account_name=self.account_name, 
+        block_blob_service = BlockBlobService(
+            account_name=self.account_name,
             account_key=self.account_key
         )
 
         expire_at = datetime.utcnow()
-        expire_at = expire_at + timedelta(seconds = 30)
-        access_policy = AccessPolicy(permission=BlobSharedAccessPermissions.WRITE, expiry=expire_at.isoformat())
-        
-        sas_token = blob_service.generate_shared_access_signature( 
-            container_name="tmp",
-            blob_name = filename, 
-            shared_access_policy=SharedAccessPolicy(access_policy)
+        expire_at = expire_at + timedelta(seconds=30)
+
+        permissions = ContainerPermissions.READ | ContainerPermissions.WRITE | ContainerPermissions.DELETE | ContainerPermissions.LIST
+
+        sas_token = block_blob_service.generate_container_shared_access_signature(
+                "tmp",
+                permission=permissions,
+                expiry=expire_at
         )
+
         return sas_token
